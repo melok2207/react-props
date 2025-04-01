c429b2f8945c73f1780e4caab86ecd21274473a9
melok2207 <132545373+melok2207@users.noreply.github.com>
2025-04-01 21:25:24 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,8 @@ const cardsData = [
 
 const App = () => {
   return (
-    <div>
-      <h1>React Props</h1>
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold text-center mb-6">React Props</h1>
       <CardList cards={cardsData} />
     </div>
   );
